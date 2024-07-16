ccffe6e149058fc9faa744a2b9f6e0b3c93da194
AbijuruT <abijuruyves@gmail.com>
2024-07-16 10:49:35 +0200

    Creating a deposit function

---
+++ index.js
@@ -0,0 +1,23 @@
+// 1. Deposit some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. Give the user their winnings
+// 7. Play again
+// In order to get the user inputs we need to use prompt-sync
+
+const userInputs = require('prompt-sync')();
+
+
+const deposit = () => {
+  const depositAmount = userInputs('Enter a deposit amount: ');
+  const numberDepositAmount = parseFloat(depositAmount);
+
+  if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+    console.log('Invalid deposit amount, try again.');
+  }
+
+  // return depositAmount;
+}
+deposit()
